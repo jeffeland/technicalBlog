afc719d7d238da79233739ddafe7eefa80db5189
Jean-Francois Landreau <jean-francois.landreau@laposte.net>
2016-05-26 09:00:22 +0200

    Update controller.js

--- controller.js
+++ controller.js
@@ -17,3 +17,21 @@ app.controller('controller', function($scope, $http, $sce) {
     })
   })
 });
+
+/*
+sampleApp .config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/addOrder', {
+        templateUrl: 'templates/add-order.html',
+        controller: 'AddOrderController'
+      }).
+      when('/showOrders', {
+        templateUrl: 'templates/show-orders.html',
+        controller: 'ShowOrdersController'
+      }).
+      otherwise({
+        redirectTo: '/addOrder'
+      });
+  }]);
+*/
