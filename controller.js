7bad90f95605b757e4fcc422e98fb8f47bd0d8b3
Jean-Francois Landreau <jean-francois.landreau@laposte.net>
2016-05-29 22:53:10 +0200

    Correction of the blogpost controller

--- controller.js
+++ controller.js
@@ -6,9 +6,9 @@
 'use strict';
 
 marked.setOptions({
-  highlight: function (code) {
-    return hljs.highlightAuto(code).value;
-  }
+    highlight: function (code) {
+        return hljs.highlightAuto(code).value;
+    }
 });
 
 var g_toc = {};
@@ -16,48 +16,47 @@ var g_toc = {};
 var app = angular.module('app', ['ngRoute']);
 
 app.config(['$routeProvider',
-  function ($routeProvider) {
-    $routeProvider.
-        when('/author', {
-          templateUrl: 'author.html',
-          controller: 'author'
-        }).
-        when('/toc', {
-          templateUrl: 'toc.html',
-          controller: 'toc'
-        }).
-        when('/blogpost/:blogPostId', {
-          templateUrl: 'blog-post.html',
-          controller: 'blogPost'
-        }).
-        otherwise({
-          redirectTo: '/toc'
-        });
-  }]);
+    function ($routeProvider) {
+        $routeProvider.
+            when('/author', {
+                templateUrl: 'author.html',
+                controller: 'author'
+            }).
+            when('/toc', {
+                templateUrl: 'toc.html',
+                controller: 'toc'
+            }).
+            when('/blogpost/:blogPostId', {
+                templateUrl: 'blog-post.html',
+                controller: 'blogPost'
+            }).
+            otherwise({
+                redirectTo: '/toc'
+            });
+    }]);
 
 app.controller('author', function ($scope, $http, $sce) {
-  $http.get('./data/author.md').success(function (authorDetails) {
-    $scope.authorDetails = $sce.trustAsHtml(marked(authorDetails));
-  });
+    $http.get('./data/author.md').success(function (authorDetails) {
+        $scope.authorDetails = $sce.trustAsHtml(marked(authorDetails));
+    });
 });
 
 app.controller('toc', function ($scope, $http) {
-  $http.get('./data/toc.json').success(function (l_toc) {
-    g_toc = l_toc;
-    $scope.toc = g_toc;
-  })
+    $http.get('./data/toc.json').success(function (l_toc) {
+        g_toc = l_toc;
+        $scope.toc = g_toc;
+    })
 });
 
-app.controller('blogPost', function ($scope, $http, $routeParams) {
-  var blogPost = {};
-  for(var i = 0; i < g_toc.length; i++) {
-    if(g_toc[i].id == $routeParams.blogPostId) {
-      blogPost = g_toc[i];
-      $http.get('./data/' + blogPost.content).success(function (blogContent) {
-              blogPost.content = $sce.trustAsHtml(marked(blogContent));
-          });
-      break;
+app.controller('blogPost', function ($scope, $http, $routeParams, $sce) {
+    for (var i = 0; i < g_toc.length; i++) {
+        if (g_toc[i].id == $routeParams.blogPostId) {
+            var blogPost = g_toc[i];
+            $http.get('./data/' + blogPost.content).success(function (blogContent) {
+                blogPost.content = $sce.trustAsHtml(marked(blogContent));
+                $scope.blogPost = blogPost;
+            });
+            break;
+        }
     }
-  }
-  $scope.blogPost = blogPost;
 });
